20e5790f952feb70d5bab99a69120d31588467f5
Ankush Goyal <ankush.go@HCL.COM>
2019-06-25 07:33:33 +0530

    introduce new food comp

--- src/Component/Card/ReceipeCard.js
+++ src/Component/Card/ReceipeCard.js
@@ -5,7 +5,9 @@ const ReceipeCard = (props) => {
     return (<div className="column">
                 <div className="receipe-card">
                     <img src={props.rList.image_url} alt="Denim Jeans" className="receipe-image"></img>
-                    <h1>{props.rList.title}</h1>
+                    <h1 className="recipes__title">{ props.rList.title.length < 20 ? `${props.rList.title}` : `${props.rList.title.substring(0, 25)}...` }</h1>
+                    <p className="recipes__subtitle">Publisher:<span>{props.rList.publisher}</span></p>
+                    <button className="recipe_buttons">View Recipe</button>
                 </div>
             </div>) 
        };
