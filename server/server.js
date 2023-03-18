b699a6e4c1b76bc4cdb9017dc90d4cd450edb683
Dev Lab <devlab@Rahuls-MacBook-Pro.local>
2023-03-18 18:01:03 +0530

    added new news api in server.js

--- server/server.js
+++ server/server.js
@@ -16,9 +16,13 @@ app.get("/api", (req, res) => {
 
 app.get("/newsapi", (req, res) => {
   const config = {};
-  config.url = "https://newsapi.org/v2/everything?q=tesla&from=2022-05-16&sortBy=publishedAt&apiKey=c438f5b301e94324a9c5609062fe4b73";
+  //[0 data coming on 2023-03] config.url = "https://newsapi.org/v2/everything?q=tesla&from=2023-03-18&sortBy=publishedAt&apiKey=c438f5b301e94324a9c5609062fe4b73";
+  // [for US]config.url = 'https://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=c438f5b301e94324a9c5609062fe4b73';
+  config.url = 'https://newsapi.org/v2/top-headlines?' + 'country=in&' + 'apiKey=c438f5b301e94324a9c5609062fe4b73';
+  // var req = new Request(url);
   config.apiKey = "c438f5b301e94324a9c5609062fe4b73";
   axios.get(config.url).then((response) => {
+    console.log(response);
     res.json(response.data);
   }).catch(e => {
     console.log("Error: ", e);
